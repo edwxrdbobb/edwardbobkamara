8dd023392df31962f5c404ee6eaf621b22f02e0b
edwxrdbobb <151927511+edwxrdbobb@users.noreply.github.com>
2024-01-30 18:56:30 +0000

    Unoptimize Images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-    /* config options here */
-  }
-   
-  module.exports = nextConfig
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: !isProd,
+  },
+  images: {
+    // Disable Image Optimization during export
+    disableStaticImages: true,
+    unoptimized: true
+  },
+};
+
+module.exports = withPWA({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp3|wav)$/i,
+      use: {
+        loader: "url-loader",
+      },
+    });
+
+    return config;
+  },
+  ...nextConfig,
+});
