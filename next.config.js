c1ce4b12eefff1c8d3a11b7c23c08d2271ec7846
edwxrdbobb <151927511+edwxrdbobb@users.noreply.github.com>
2024-01-30 21:03:21 +0000

    basepath set to repo name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require("next-pwa");
 
 const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? '/your-repository-name' : '';
+const basePath = isProd ? '/edwardbobkamara' : '';
 
 const nextConfig = {
   basePath,
