418e7e9f2da313c9f057ce3645ee30b1c2c17953
edwxrdbobb <151927511+edwxrdbobb@users.noreply.github.com>
2024-01-30 23:39:54 +0000

    Update ProjectTile.js

--- components/Projects/ProjectTile/ProjectTile.js
+++ components/Projects/ProjectTile/ProjectTile.js
@@ -82,7 +82,7 @@ const ProjectTile = ({ project, classes, isDesktop }) => {
             {project.tech.map((el, i) => (
               <img
                 className={`${i % 2 === 0 && "ml-16"} mb-4`}
-                src={`/projects/tech/${el}.png`}
+                src={`./projects/tech/${el}.png`}
                 alt={el}
                 height={45}
                 width={45}
